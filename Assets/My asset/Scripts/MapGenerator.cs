aac8e7ce8bfa58aefcd383a586708d44357e90f9
kenolion <kenolion13@gmail.com>
2018-08-27 14:52:47 +0800

    testtsts

--- Assets/My asset/Scripts/MapGenerator.cs
+++ Assets/My asset/Scripts/MapGenerator.cs
@@ -11,7 +11,7 @@ public class MapGenerator : MonoBehaviour
 
     delegate void MyDelegate();
     MyDelegate myDelegate;
-
+    public BoundsInt area;
 
     // Use this for initialization
     void Start()
@@ -46,6 +46,24 @@ public class MapGenerator : MonoBehaviour
         if (Input.GetMouseButtonDown(0))
         {
             tilemaps[1].SetTile(tilemaps[1].WorldToCell(worldPoint), boxfillTile);
+            BoundsInt bounds = tilemaps[1].cellBounds;
+            TileBase[] allTiles = tilemaps[1].GetTilesBlock(bounds);
+
+            for (int x = 0; x < bounds.size.x; x++)
+            {
+                for (int y = 0; y < bounds.size.y; y++)
+                {
+                    TileBase tile = allTiles[x + y * bounds.size.x];
+                    if (tile != null)
+                    {
+                        Debug.Log("x:" + x + " y:" + y + " tile:" + tile.name);
+                    }
+                    else
+                    {
+                        Debug.Log("x:" + x + " y:" + y + " tile: (null)");
+                    }
+                }
+            }
         }
 
 

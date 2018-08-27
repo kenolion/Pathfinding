3acbde59de2c1a03fbcbedccfd65af67359a86fd
kenolion <kenolion13@gmail.com>
2018-08-27 13:17:34 +0800

    test

---
+++ Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class MapGenerator : MonoBehaviour
+{
+
+    public Tilemap[] tilemaps;
+    public Tile boxfillTile;
+
+    delegate void MyDelegate();
+    MyDelegate myDelegate;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        //tilemaps[0].BoxFill(new Vector3Int(0, 0, 0), boxfillTile, -5, -5, 10, 10);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (myDelegate != null)
+            myDelegate();
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            myDelegate -= placeTiles;
+            myDelegate += placeTiles;
+        }
+        else if (Input.GetKeyDown(KeyCode.P))
+        {
+            myDelegate -= placeTiles;
+
+        }
+
+    }
+
+    void placeTiles()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Vector3 worldPoint = ray.GetPoint(-ray.origin.z / ray.direction.z);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            tilemaps[1].SetTile(tilemaps[1].WorldToCell(worldPoint), boxfillTile);
+        }
+
+
+    }
+
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    Rigidbody2D rigid;
+    public Vector2 speed;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        rigid = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            rigid.rotation = 90;
+        }
+        else if (Input.GetAxis("Horizontal") < 0)
+        {
+            rigid.rotation = -90;
+        }
+        if (Input.GetAxis("Vertical") > 0)
+        {
+            rigid.rotation = 180;
+        }
+        else if (Input.GetAxis("Vertical") < 0)
+        {
+            rigid.rotation = 0;
+        }
+        rigid.velocity = -transform.up * speed;
+    }
+}
